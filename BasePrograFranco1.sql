75165c91134dcb66afb3cfb4c0b65d3bf7b635e3
jusola2 <juanjosesolano@hotmail.com>
2018-03-06 22:14:19 -0600

    A
    avance

--- BasePrograFranco1.sql
+++ BasePrograFranco1.sql
@@ -7,4 +7,30 @@ USE [BD_sistemaEscolar]
 CREATE TABLE [dbo].[Estudiante](
     [IdGrupo] [int] identity (1,1) NOT NULL PRIMARY KEY,
 	[Nombre][nvarchar](150) NOT NULL,
-    [CodigoGrupo] [nvarchar](100) NULL)
+	[Email][nvarchar](150)  NULL,
+	[Telefono][int] NULL,
+    [Estado] [nvarchar](10) NOT NULL)--Acti=activo, RetJ=retiro justificado, RetI= retiro injustificado
+
+
+USE [BD_sistemaEscolar]
+
+CREATE TABLE [dbo].[PeriodoLectivo](
+    [IdPeriodo] [int] identity (1,1) NOT NULL PRIMARY KEY,
+	[FechaIni][DATETIME] NOT NULL,
+    [FechaFin] [datetime] NULL)
+
+
+USE [BD_sistemaEscolar]
+
+CREATE TABLE [dbo].[Curso](
+    [IdCurso] [int] identity (1,1) NOT NULL PRIMARY KEY,
+	[IdPeriodo][int]  FOREIGN KEY REFERENCES PeriodoLectivo(IdPeriodo),
+    [Estado] [nvarchar](10) NOT NULL)--cre=creado,eje=ejecución, anu=anulado y fin=finalizado
+
+
+USE [BD_sistemaEscolar]
+
+CREATE TABLE [dbo].[Curso](
+    [IdCurso] [int] identity (1,1) NOT NULL PRIMARY KEY,
+	[IdPeriodo][int]  FOREIGN KEY REFERENCES PeriodoLectivo(IdPeriodo),
+    [Estado] [nvarchar](10) NOT NULL)--cre=creado,eje=ejecución, anu=anulado y fin=finalizado
