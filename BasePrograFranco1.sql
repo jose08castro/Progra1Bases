11827602d268860387c168a768125e1beec9b692
jusola2 <juanjosesolano@hotmail.com>
2018-04-14 23:17:01 -0600

    V 3.1
    MAE , EL SCRIPT YA CARGA EL XML, YO USE EL 8, SOLO CORRANLO 1 VEZ

--- BasePrograFranco1.sql
+++ BasePrograFranco1.sql
@@ -12,7 +12,7 @@ CREATE TABLE [dbo].[Estudiante](
 	[Email][nvarchar](150)  NOT NULL,
 	[Password][nvarchar](150) NOT NULL,
 	[Telefono][int] NULL,
-    [Estado] [nvarchar](10) NOT NULL)--Acti=activo, RetJ=retiro justificado, RetI= retiro injustificado
+    [Estado] [nvarchar](10) NOT NULL)
 
 
 USE [BD_sistemaEscolar]
@@ -23,22 +23,21 @@ CREATE TABLE [dbo].[Profesor](
 	[Email][nvarchar](150)  NULL,
 	[Password][nvarchar](150) NOT NULL)
 
+USE [BD_sistemaEscolar]
+
+CREATE TABLE [dbo].[Periodo_Lectivo](
+    [IdPeriodo] [int] identity (1,1) NOT NULL PRIMARY KEY,
+	[Inicio] [datetime] NOT NULL,
+	[Fin] [datetime] NOT NULL,
+    [Activo] [nvarchar](5) NOT NULL)
+
 
 USE [BD_sistemaEscolar]
 
 CREATE TABLE [dbo].[Rubro](
     [IdRubro] [int] identity (1,1) NOT NULL PRIMARY KEY,
-	[Nombre][nvarchar](150) NOT NULL,
-    [Instancia] [nvarchar](10) NOT NULL)--fijo, vari= variable
+	[Nombre][nvarchar](150) NOT NULL)
 
-
-USE [BD_sistemaEscolar]
-
-CREATE TABLE [dbo].[Evaluacion](
-    [IdEvaluacion] [int] identity (1,1) NOT NULL PRIMARY KEY,
-	[IdRubro][int]  FOREIGN KEY REFERENCES Rubro(IdRubro),
-    [Numero] [tinyint] NOT NULL,
-	[Porcentaje] [tinyint] NOT NULL)
 
 USE [BD_sistemaEscolar]
 
@@ -50,8 +49,7 @@ USE [BD_sistemaEscolar]
 
 CREATE TABLE [dbo].[Grupo](
     [IdGrupo] [int] identity (1,1) NOT NULL PRIMARY KEY,
-	[IdEvaluacion] [int] FOREIGN KEY REFERENCES Evaluacion(IdEvaluacion),
-	[Periodo] [int] NOT NULL,
+	[IdPeriodo] [int] FOREIGN KEY REFERENCES Periodo_Lectivo(IdPeriodo),
 	[Codigo] [nvarchar](50) NOT NULL,
 	[IdProfesor][int] FOREIGN KEY REFERENCES Profesor(IdProfesor),
 	[Nombre][nvarchar](150) NOT NULL,
@@ -60,26 +58,41 @@ CREATE TABLE [dbo].[Grupo](
 
 USE [BD_sistemaEscolar]
 
+CREATE TABLE [dbo].[Config_Evaluacion](
+    [IdConfig_Evaluacion] [int] identity (1,1) NOT NULL PRIMARY KEY,
+	[IdRubro][int]  FOREIGN KEY REFERENCES Rubro(IdRubro),
+	[IdGrupo][int]  FOREIGN KEY REFERENCES Grupo(IdGrupo),
+    [Numero] [tinyint] NOT NULL,
+	[Porcentaje] [float] NOT NULL,
+	[Constante] [nvarchar](5) NOT NULL)
+
+USE [BD_sistemaEscolar]
+
+CREATE TABLE [dbo].[Evaluacion](
+    [IdEvaluacion] [int] identity (1,1) NOT NULL PRIMARY KEY,
+	[IdConfig_Evaluacion][int]  FOREIGN KEY REFERENCES Config_Evaluacion(IdConfig_Evaluacion),
+	[Nombre][nvarchar](25) NOT NULL ,
+    [Fecha] [datetime] NOT NULL,
+	[Porcentaje] [float] NOT NULL,
+	[Descripcion] [nvarchar](200) NOT NULL)
+
+USE [BD_sistemaEscolar]
+
 CREATE TABLE [dbo].[Status_Estudiante](
     [IdStatus_Estudiante] [int] identity (1,1) NOT NULL PRIMARY KEY,
 	[Nombre] [nvarchar](50) NOT NULL)
 
 USE [BD_sistemaEscolar]
-
-CREATE TABLE [dbo].[EstudianteXGrupo](
-    [IdGrupo] [int]  FOREIGN KEY REFERENCES Grupo(IdGrupo) PRIMARY KEY,
+CREATE TABLE [dbo].[EstudiantesXGrupo](
+	[Id] [int] identity (1,1) NOT NULL PRIMARY KEY,
+    [IdGrupo] [int]  FOREIGN KEY REFERENCES Grupo(IdGrupo),
 	[IdEstudiante][int]  FOREIGN KEY REFERENCES Estudiante(IdEstudiante),
-	[Status][int]  FOREIGN KEY REFERENCES Status_Estudiante(IdStatus_Estudiante))
-
-
-USE [BD_sistemaEscolar]
+	[NotaTotal] [float] NOT NULL,
+	[Status][int]FOREIGN KEY REFERENCES Status_Estudiante(IdStatus_Estudiante))
 
 CREATE TABLE [dbo].[NotasXGrupo](
-    [Id] [int] identity (1,1) NOT NULL PRIMARY KEY,
-	[IdGrupo] [int] FOREIGN KEY REFERENCES EstudianteXGrupo(IdGrupo),
-	[IdEvalucacion][int] FOREIGN KEY REFERENCES Evaluacion(IdEvaluacion),
-	[IdEstudiante][nvarchar](150) NOT NULL,
-	[Rubro][int] NOT NULL,
-	[Numero][tinyint] NOT NULL,
-    [Valor] [tinyint] NOT NULL,
-	[Obtenido] [tinyint] NULL)
+	[IdNotas] [int] identity (1,1) NOT NULL PRIMARY KEY,
+    [IdEstudiantesXGrupo] [int]  FOREIGN KEY REFERENCES EstudiantesXGrupo(Id),
+	[IdEvaluacion][int]  FOREIGN KEY REFERENCES Evaluacion(IdEvaluacion),
+	[Obtenido] [float] NOT NULL)
+
--- ScriptInsertarXml.sql
+++ ScriptInsertarXml.sql
@@ -27,8 +27,8 @@ Studentphone [int] '@phone'
 
 USE  [BD_sistemaEscolar]
 
---insert into dbo.Estudiante(Nombre,Email,Password,Telefono,Estado)
---select * from @tempo
+insert into dbo.Estudiante(Nombre,Email,Password,Telefono,Estado)
+select * from @tempo
 select * from dbo.Estudiante
 EXEC sp_xml_removedocument @hDoc
 GO
@@ -58,8 +58,8 @@ TeacherPassword [nvarchar](150) '@password'
 
 USE  [BD_sistemaEscolar]
 
---insert into dbo.Profesor(Nombre,Email,Password)
---select * from @tempo
+insert into dbo.Profesor(Nombre,Email,Password)
+select * from @tempo
 
 select * from dbo.Profesor
 
@@ -90,8 +90,8 @@ StatusName [nvarchar](50) '@name'
 
 USE  [BD_sistemaEscolar]
 
---insert into dbo.Status_Estudiante(Nombre)
---select * from @tempo
+insert into dbo.Status_Estudiante(Nombre)
+select * from @tempo
 
 select * from dbo.Status_Estudiante
 
@@ -122,10 +122,254 @@ StatusName [nvarchar](50) '@name'
 
 USE  [BD_sistemaEscolar]
 --select * from @tempo
---insert into dbo.Status_Grupo(Nombre)
---select * from @tempo
+insert into dbo.Status_Grupo(Nombre)
+select * from @tempo
 
 select * from dbo.Status_Grupo
+
+EXEC sp_xml_removedocument @hDoc
+GO
+
+--para periodo lectivo 
+
+USE  OPENXMLTesting
+GO
+
+declare @tempo as table(Inicio datetime, Fin datetime, Activo nvarchar(5))
+
+DECLARE @XML AS XML, @hDoc AS INT, @SQL NVARCHAR (MAX)
+
+
+SELECT @XML = XMLData FROM  XMLwithOpenXML
+
+EXEC sp_xml_preparedocument @hDoc OUTPUT, @XML
+
+insert into @tempo
+SELECT  Inicio,Fin,Activo
+FROM OPENXML(@hDoc, 'XML/termData/term')
+WITH 
+(
+Inicio [datetime] '@start',
+Fin [datetime] '@end',
+Activo [nvarchar](5) '@active'
+);
+
+USE  [BD_sistemaEscolar]
+--select * from @tempo
+insert into dbo.Periodo_Lectivo(Inicio,Fin,Activo)
+select * from @tempo
+
+select * from dbo.Periodo_Lectivo
+
+EXEC sp_xml_removedocument @hDoc
+GO
+
+--para rubro
+
+USE  OPENXMLTesting
+GO
+
+declare @tempo as table(Nombre nvarchar(150))
+
+DECLARE @XML AS XML, @hDoc AS INT, @SQL NVARCHAR (MAX)
+
+
+SELECT @XML = XMLData FROM  XMLwithOpenXML
+
+EXEC sp_xml_preparedocument @hDoc OUTPUT, @XML
+
+insert into @tempo
+SELECT  Nombre
+FROM OPENXML(@hDoc, 'XML/itemData/item')
+WITH 
+(
+Nombre [nvarchar](150) '@name'
+);
+
+USE  [BD_sistemaEscolar]
+--select * from @tempo
+insert into dbo.Rubro(Nombre)
+select * from @tempo
+
+select * from dbo.Rubro
+
+EXEC sp_xml_removedocument @hDoc
+GO
+
+
+--para Grupo
+
+USE  OPENXMLTesting
+GO
+
+declare @tempo as table( IdPeriodo int, Codigo nvarchar(50), IdProfesor int, Nombre nvarchar(150), Status int)
+
+DECLARE @XML AS XML, @hDoc AS INT, @SQL NVARCHAR (MAX)
+
+
+SELECT @XML = XMLData FROM  XMLwithOpenXML
+
+EXEC sp_xml_preparedocument @hDoc OUTPUT, @XML
+
+insert into @tempo
+SELECT  IdPeriodo , Codigo , IdProfesor , Nombre , Status 
+FROM OPENXML(@hDoc, 'XML/groupData/group')
+WITH 
+(
+IdPeriodo [int] '@termID',
+Codigo [nvarchar](50) '@code',
+IdProfesor [int] '@teacherID',
+Nombre [nvarchar](150) '@courseName',
+Status [int] '@groupStateID'
+);
+
+USE  [BD_sistemaEscolar]
+--select * from @tempo
+insert into dbo.Grupo( IdPeriodo,  Codigo , IdProfesor , Nombre , Status )
+select * from @tempo
+
+select * from dbo.Grupo
+
+EXEC sp_xml_removedocument @hDoc
+GO
+
+--para Config_Evaluacion
+
+USE  OPENXMLTesting
+GO
+
+declare @tempo as table(IdRubro int, IdGrupo int, Numero tinyint, Porcentaje float,Constante nvarchar(5))
+
+DECLARE @XML AS XML, @hDoc AS INT, @SQL NVARCHAR (MAX)
+
+
+SELECT @XML = XMLData FROM  XMLwithOpenXML
+
+EXEC sp_xml_preparedocument @hDoc OUTPUT, @XML
+
+insert into @tempo
+SELECT  IdRubro, IdGrupo, Numero , Porcentaje ,Constante 
+FROM OPENXML(@hDoc, 'XML/groupItemData/groupItem ')
+WITH 
+(
+IdRubro [int] '@itemID',
+IdGrupo [int] '@groupID',
+Numero [tinyint] '@count',
+Porcentaje [float] '@percentage',
+Constante [nvarchar](5) '@constantCount'
+);
+
+USE  [BD_sistemaEscolar]
+--select * from @tempo
+insert into dbo.Config_Evaluacion(IdRubro, IdGrupo , Numero , Porcentaje ,Constante )
+select * from @tempo
+
+select * from dbo.Config_Evaluacion
+
+EXEC sp_xml_removedocument @hDoc
+GO
+
+
+--para Evaluacion
+
+USE  OPENXMLTesting
+GO
+
+declare @tempo as table(IdConfig_Evaluacion int, Nombre nvarchar(25), Fecha datetime, Porcentaje float,Descripcion nvarchar(200))
+
+DECLARE @XML AS XML, @hDoc AS INT, @SQL NVARCHAR (MAX)
+
+
+SELECT @XML = XMLData FROM  XMLwithOpenXML
+
+EXEC sp_xml_preparedocument @hDoc OUTPUT, @XML
+
+insert into @tempo
+SELECT  IdConfig_Evaluacion , Nombre , Fecha , Porcentaje ,Descripcion 
+FROM OPENXML(@hDoc, 'XML/evaluationData/evaluation')
+WITH 
+(
+IdConfig_Evaluacion [int] '@groupItemID',
+Nombre [nvarchar](25) '@name',
+Fecha [datetime] '@date',
+Porcentaje [float] '@percentage',
+Descripcion [nvarchar](200) '@description'
+);
+
+USE  [BD_sistemaEscolar]
+--select * from @tempo
+insert into dbo.Evaluacion(IdConfig_Evaluacion , Nombre , Fecha , Porcentaje ,Descripcion)
+select * from @tempo
+
+select * from dbo.Evaluacion
+
+EXEC sp_xml_removedocument @hDoc
+GO
+--para EstudiantesXgrupo
+
+USE  OPENXMLTesting
+GO
+
+declare @tempo as table(IdGrupo  int, IdEstudiante int, NotaTotal float, Status int)
+
+DECLARE @XML AS XML, @hDoc AS INT, @SQL NVARCHAR (MAX)
+
+
+SELECT @XML = XMLData FROM  XMLwithOpenXML
+
+EXEC sp_xml_preparedocument @hDoc OUTPUT, @XML
+
+insert into @tempo
+SELECT  IdGrupo  , IdEstudiante , NotaTotal , Status 
+FROM OPENXML(@hDoc, 'XML/studentGroupData/studentGroup')
+WITH 
+(
+IdGrupo [int] '@groupID',
+IdEStudiante [int] '@studentID',
+NotaTotal [float] '@totalGrade',
+Status [int] '@studentGroupStateID'
+);
+
+USE  [BD_sistemaEscolar]
+--select * from @tempo
+insert into dbo.EstudiantesXGrupo(IdGrupo  , IdEstudiante , NotaTotal , Status)
+select * from @tempo
+
+select * from dbo.EstudiantesXGrupo
+
+EXEC sp_xml_removedocument @hDoc
+GO
+
+--para notasxgrupo
+
+USE  OPENXMLTesting
+GO
+
+declare @tempo as table(IdEstudintesXGrupo  int, IdEvaluacion int, Obtenido float)
+
+DECLARE @XML AS XML, @hDoc AS INT, @SQL NVARCHAR (MAX)
+
+
+SELECT @XML = XMLData FROM  XMLwithOpenXML
+
+EXEC sp_xml_preparedocument @hDoc OUTPUT, @XML
+
+insert into @tempo
+SELECT  IdEstudintesXGrupo , IdEvaluacion , Obtenido 
+FROM OPENXML(@hDoc, 'XML/studentEvaluationData/studentEvaluation')
+WITH 
+(
+IdEstudintesXGrupo [int] '@studentGroupID',
+IdEvaluacion [int] '@evaluationID',
+Obtenido [float] '@grade'
+);
+
+USE  [BD_sistemaEscolar]
+--select * from @tempo
+insert into dbo.NotasXGrupo(IdEstudiantesXGrupo, IdEvaluacion, Obtenido)
+select * from @tempo
+
+select * from dbo.NotasXGrupo
 
 EXEC sp_xml_removedocument @hDoc
 GO
