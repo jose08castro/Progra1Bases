c5592e7019790965887721a9ee281e8a65cd4513
jusola2 <juanjosesolano@hotmail.com>
2018-04-19 11:04:59 -0600

    querys v1.3
    sad

--- PRUEBAS_SP.sql
+++ PRUEBAS_SP.sql
@@ -1,3 +1,4 @@
+--1
 Use [BD_sistemaEscolar]
 
 go 
@@ -7,6 +8,24 @@ DECLARE @Con nvarchar(150)
 set @Ema='TauoheAruokego56@ic-itcr.ac.cr'
 set @Con='2015153972'
 
-EXEC Obtner_Id_Estudiante @Ema,@Con,@Resultado=@resultado OUTPUT;
+EXEC Obtener_Id_Estudiante @Ema,@Con,@Resultado=@resultado OUTPUT;
 
 select @resultado
+
+
+--2
+Use [BD_sistemaEscolar]
+
+go 
+declare @resultado int
+DECLARE @Ema nvarchar(150)
+DECLARE @Con nvarchar(150)
+set @Ema='FonogukaRaalupi62@yahoo.com'
+set @Con='9397571861'
+
+EXEC Obtener_Id_Profesor @Ema,@Con,@Resultado=@resultado OUTPUT;
+
+select @resultado
+
+
+select * from Profesor
--- QuerysDeLaProgra.sql
+++ QuerysDeLaProgra.sql
@@ -7,10 +7,10 @@ SET QUOTED_IDENTIFIER ON
 GO
 -- =============================================
 -- Author:		<00776dc467f2b588b23350d2db96c58163ecbca282e7199a6d7746542d1b30ad>
--- Create date: <11/19/17>
--- Description:	<Crear actividad>
+-- Create date: <10/4/18>
+-- Description:	<Obtener id de los estudiantes>
 -- =============================================
-CREATE PROCEDURE Obtner_Id_Estudiante(
+CREATE PROCEDURE Obtener_Id_Estudiante(
 	-- Add the parameters for the stored procedure here
 	@Email nvarchar(150),
 	@Contrasenna nvarchar(150), 
@@ -23,6 +23,36 @@ BEGIN
 	declare @Result int
 	SET NOCOUNT ON;
 	Set @Result = (select E.IdEstudiante from Estudiante E where E.Email=@Email and E.Password=@Contrasenna)
+	set @Resultado = ISNULL(@Result, -1)
+	RETURN @Resultado
+END
+GO
+
+-- 2
+
+Use [BD_sistemaEscolar]
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<00776dc467f2b588b23350d2db96c58163ecbca282e7199a6d7746542d1b30ad>
+-- Create date: <10/4/18>
+-- Description:	<Obtener id de los profesores>
+-- =============================================
+CREATE PROCEDURE Obtener_Id_Profesor(
+	-- Add the parameters for the stored procedure here
+	@Email nvarchar(150),
+	@Contrasenna nvarchar(150), 
+	@Resultado int OUTPUT
+	)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	declare @Result int
+	SET NOCOUNT ON;
+	Set @Result = (select P.IdProfesor from Profesor P where P.Email=@Email and P.Password=@Contrasenna)
 	set @Resultado = ISNULL(@Result, -1)
 	RETURN @Resultado
 END
