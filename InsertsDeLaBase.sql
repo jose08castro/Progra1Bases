7a9e4cb483bb2083fa5a913ebec9d804409662e1
jusola2 <juanjosesolano@hotmail.com>
2018-04-19 23:59:20 -0600

    k
    ds

--- InsertsDeLaBase.sql
+++ InsertsDeLaBase.sql
@@ -33,17 +33,17 @@ BEGIN
 			BEGIN
 				Insert Into DBO.Estudiante(Nombre,Apellido,Email,Password,Carnet,Telefono)
 				Values(@Nombre,@Apellido,@Email,@Password,@Carnet,@Telefono)
-				set @Result = 1
+				set @Result = 1 --correcto
 			END
 		ELSE
 			BEGIN 
 				rollback TRAN insertar
-				SET @Result = -2
+				SET @Result = -2--ya existe estudiante con ese correo
 			END
 		end try 
 		begin catch
 		rollback TRAN insertar
-		set @Result = -1
+		set @Result = -1--hubo un problema 
 		end catch	
 	if(@Result !=-1 AND @Result !=-2)
 	begin
@@ -65,14 +65,12 @@ GO
 -- Create date: <14/4/17>
 -- Description:	<Insert Profesor>
 -- =============================================
-CREATE PROCEDURE Insertar_Estudiante(
+CREATE PROCEDURE Insertar_Profesor(
 	-- Add the parameters for the stored procedure here
 	@Nombre nvarchar(150),
 	@Apellido nvarchar(150) ,
 	@Email nvarchar(150),
 	@Password nvarchar(150),
-	@Carnet int,
-	@Telefono int,
 	@Result int OUTPUT
 	)
 AS
@@ -83,22 +81,22 @@ BEGIN
 	SET NOCOUNT ON;
 	BEGIN TRAN insertar
 		begin try
-		Set @Existe = (Select E.IdEstudiante from Estudiante E where E.Email=@Email)
+		Set @Existe = (Select p.IdProfesor from Profesor P where P.Email=@Email)
 		IF @Existe IS NULL
 			BEGIN
-				Insert Into DBO.Estudiante(Nombre,Apellido,Email,Password,Carnet,Telefono)
-				Values(@Nombre,@Apellido,@Email,@Password,@Carnet,@Telefono)
-				set @Result = 1
+				Insert Into DBO.Profesor(Nombre,Apellido,Email,Password)
+				Values(@Nombre,@Apellido,@Email,@Password)
+				set @Result = 1--correcto
 			END
 		ELSE
 			BEGIN 
 				rollback TRAN insertar
-				SET @Result = -2
+				SET @Result = -2--ya existe
 			END
 		end try 
 		begin catch
 		rollback TRAN insertar
-		set @Result = -1
+		set @Result = -1--hubo un problema
 		end catch	
 	if(@Result !=-1 AND @Result !=-2)
 	begin
@@ -108,3 +106,54 @@ BEGIN
 END
 GO
 
+-- 
+
+Use [BD_sistemaEscolar]
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<00776dc467f2b588b23350d2db96c58163ecbca282e7199a6d7746542d1b30ad>
+-- Create date: <14/4/17>
+-- Description:	<Crear periodo lectivo>
+-- =============================================
+CREATE PROCEDURE Crear_Periodo(
+	-- Add the parameters for the stored procedure here
+	@Inicio datetime,
+	@Fin datetime,
+	@Activo nvarchar(5),
+	@Result int OUTPUT
+	)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	declare @Existe int 
+	SET NOCOUNT ON;
+	BEGIN TRAN insertar
+		begin try
+		Set @Existe = (Select p.IdProfesor from Profesor P where P.Email=@Email)
+		IF @Existe IS NULL
+			BEGIN
+				Insert Into DBO.Profesor(Nombre,Apellido,Email,Password)
+				Values(@Nombre,@Apellido,@Email,@Password)
+				set @Result = 1--correcto
+			END
+		ELSE
+			BEGIN 
+				rollback TRAN insertar
+				SET @Result = -2--ya existe
+			END
+		end try 
+		begin catch
+		rollback TRAN insertar
+		set @Result = -1--hubo un problema
+		end catch	
+	if(@Result !=-1 AND @Result !=-2)
+	begin
+	commit tran insertar
+	end
+	return @Result
+END
+GO
